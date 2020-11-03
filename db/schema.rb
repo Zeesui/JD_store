8655b7a29229952b67a14080f8ebfcad50e61e42
zee <kooo9671354@gmail.com>
2020-11-03 14:27:07 +0800

    add aasm_state

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_014911) do
+ActiveRecord::Schema.define(version: 2020_11_03_031711) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2020_10_30_014911) do
     t.string "token"
     t.boolean "is_paid", default: false
     t.string "payment_method"
+    t.string "aasm_state", default: "order_placed"
+    t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
   create_table "product_lists", force: :cascade do |t|
