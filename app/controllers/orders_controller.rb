795cfcaa236b7c292ee3e32eb8de181285d2ed7b
zee <kooo9671354@gmail.com>
2020-11-03 11:13:48 +0800

    change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,12 +11,13 @@ class OrdersController < ApplicationController
 		if @order.save
 			@items.each do |item|
 				product_list = ProductList.new
-				product_list.order = @order 
+				product_list.order = @order
 				product_list.product_name = item.product.title
 				product_list.product_price = item.product.price
-				product_list.quantity = item.quantity 
+				product_list.quantity = item.quantity
 				product_list.save
 			end
+			OrderMailer.notify_order_placed(@order).deliver!
 			redirect_to order_path(@order.token)
 		else
 			render 'carts/checkout'
