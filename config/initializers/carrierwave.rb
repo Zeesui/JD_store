aa94d44b21e8a2d843e621fadefe5de217cecf9a
zee <kooo9671354@gmail.com>
2020-12-09 17:02:37 +0800

    figaro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.storage = :qiniu
-  config.qiniu_acess_key = ENV["qiniu_access_key"]
+  config.qiniu_access_key = ENV["qiniu_access_key"]
   config.qiniu_secret_key = ENV["qiniu_secret_key"]
   config.qiniu_bucket = ENV["qiniu_bucket"]
   config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
