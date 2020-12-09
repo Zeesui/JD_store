6f99530356bd6fe7fdb0f3d1ad9190f8c5308038
zee <kooo9671354@gmail.com>
2020-12-09 22:03:09 +0800

    change

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,5 +6,5 @@ CarrierWave.configure do |config|
  config.qiniu_bucket_domain = ENV["https://portal.qiniu.com/cdn/domain/ql2av6kts.hn-bkt.clouddn.com"]
  config.qiniu_block_size    = 4*1024*1024
  config.qiniu_protocol      = "https"
- config.qiniu_up_host       = "https://portal.qiniu.com"  #选择不同的区域时，"up.qiniu.com" 不同
+ config.qiniu_up_host       = "portal.qiniu.com"  #选择不同的区域时，"up.qiniu.com" 不同
 end
