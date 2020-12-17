e17a1b804ee3f374f0db7c805ddc488587c16ac6
zee <kooo9671354@gmail.com>
2020-12-17 15:06:54 +0800

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://zee-jdstore.herokuapp.com'}
 
-  config.action_mailer.delivery_method = :smtp
+#  config.action_mailer.delivery_method = :smtp
 
 end
